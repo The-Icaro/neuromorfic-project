b62c86606cb2a6cca74005b44fac0588f781bed2
The-Icaro <icarofigaro100@hotmail.com>
2023-04-13 22:02:36 -0300

    feat: add redis cache and routes

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseInterceptors,
+  CacheInterceptor,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/storedata/')
+  async findOne(@Query('id') id: string) {
+    return await this.appService.getCacheData(id);
+  }
+
+  @UseInterceptors(CacheInterceptor)
+  @Post('/storedata/')
+  async storeNewData(
+    @Body() storeNewData: { dataId: string; maxSize: number },
+  ) {
+    const { dataId, maxSize } = storeNewData;
+    return await this.appService.storeExternalData(dataId, maxSize);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, CACHE_MANAGER } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly httpService: HttpService,
+    @Inject(CACHE_MANAGER) private cacheService: Cache,
+  ) {}
+
+  async storeExternalData(dataId: string, maxSize: number) {
+    const cachedData = await this.getCacheData(dataId);
+
+    if (cachedData) return cachedData;
+
+    const { data } = await this.httpService.axiosRef.get(
+      `https://api.thingspeak.com/channels/${dataId}/feeds.json?results=${maxSize}`,
+    );
+
+    return await this.setCacheData(dataId, data);
+  }
+
+  async getCacheData(key: string) {
+    if (!key) return [];
+    return await this.cacheService.get(key);
+  }
+
+  async setCacheData(key: string, data: any) {
+    if (!key || !data) return [];
+
+    await this.cacheService.set(key, data, 0);
+
+    return await this.cacheService.get(key);
   }
 }
