ab365c6e608ba2c98aaadad634bfd612c95c5811
Icaro <84683833+The-Icaro@users.noreply.github.com>
2023-04-13 22:09:04 -0300

    feat: add redis cache and routes
    * feat: add nestjs backend structure
    
    * feat: add redis cache and routes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import type { RedisClientOptions } from 'redis';
+import { redisStore } from 'cache-manager-redis-yet';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule,
+    ConfigModule.forRoot(),
+    CacheModule.register<RedisClientOptions>({
+      isGlobal: true,
+      store: redisStore,
+      url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
